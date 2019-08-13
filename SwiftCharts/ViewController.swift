270cba07e75cdfe930876a982922bc718d865513
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2019-08-13 15:49:39 -0300

    initial commit 🎉

--- SwiftCharts/ViewController.swift
+++ SwiftCharts/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    var xData = PieChartDataEntry(value: 0.0)
+    var yData = PieChartDataEntry(value: 0.0)
+    var zData = PieChartDataEntry(value: 0.0)
+    
+    var dataEntries = [PieChartDataEntry]()
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        prepareChart()
+        
+    }
+    
+    // MARK: - Chart Helper
+    private func prepareChart() {
+        pieChart.backgroundColor = UIColor.blue
+        pieChart.holeColor = UIColor.brown
+        
+        // doghtnut hole
+        pieChart.drawHoleEnabled = true
+        pieChart.highlightPerTapEnabled = false
+        
+        // auto spin
+        pieChart.spin(duration: 0.38, fromAngle: 0, toAngle: 720)
+        
+        // user cannot spin
+        pieChart.isUserInteractionEnabled = false
+    
+        pieChart.chartDescription?.text = ""
+    
+        // ------
+        xData.value = 130
+        xData.label = "X"
+        
+        yData.value = 20
+        yData.label = "Y"
+        
+        zData.value = 50
+        zData.label = "Z"
+        
+        
+        // ------
+        dataEntries = [xData, yData, zData]
+        updateChartData()
     }
 
-
+    private func updateChartData() {
+        let chartDataSet = PieChartDataSet(entries: dataEntries, label: "")
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor.red, UIColor.orange, UIColor.purple]
+        chartDataSet.colors = colors
+        
+        
+        //
+        pieChart.data = chartData
+        
+    }
+    
 }
 
